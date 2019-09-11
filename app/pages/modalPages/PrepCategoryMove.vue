2776c92d0ce0693c2a09e2f4778b5436a400af8d
Hans Bakker <admin@antwebsystems.com>
2019-09-11 12:59:11 +0700

    small adjustment of last commit

--- app/pages/modalPages/PrepCategoryMove.vue
+++ app/pages/modalPages/PrepCategoryMove.vue
@@ -1,17 +1,18 @@
 <template>
   <page><ModalStack dismissEnabled="true" class="modal-container">
-    <StackLayout class="p-20" backgroundColor="white" paddingTop="40">
-      <Button class="button" :text="$t('cancel')" @tap="cancelMove()"/>
-      <Label :text="$t('movePrepCat')" textWrap="true"/>
-      <RadListView ref="listViewItem" for="item in items" @itemTap="onItemTap">
+    <StackLayout backgroundColor="white" width="90%" padding="20">
+      <Label :text="name" horizontalAlignment="center" class="h2"/>
+      <Label :text="$t('movePrepCat')" textWrap="true" class="h3"
+          horizontalAlignment="center" padding="10"/>
+      <RadListView for="item in items" @itemTap="onItemTap" height="50%">
         <v-template>
-          <GridLayout columns="50, *, auto" rows="*" class="item"
-                paddingRight="5" paddingLeft="5">
+          <GridLayout columns="50, *, auto" rows="*">
             <Image :src="item.image"  col="0" class="thumbnail"/>
-            <label :text="item.description" class="button" col="1"/>
+            <label :text="item.description" class="h2" col="1"/>
           </GridLayout>
         </v-template>
       </RadListView>
+      <Button :text="$t('cancel')" @tap="$modal.close()" class="button"/>
     </StackLayout></ModalStack>
   </page>
 </template>
@@ -19,7 +20,11 @@
 <script>
 export default {
     name: 'PrepCategoryMove',
-    props: [ 'prepId', 'catId'],
+    props: {
+      prepId: String,
+      catId : String,
+      name: String
+    },
     data() {
       return {
         itemsAll: this.$store.getters.prepAreasAndCategories(),
@@ -33,6 +38,7 @@ export default {
     },
     methods: {
       onItemTap(args) {
+        console.log("===move: args" + args.item.description)
         this.$backendService.movePreparationAreaCategory(
             this.prepId, args.item.preparationAreaId, this.catId)
         .then(() => {
@@ -40,9 +46,6 @@ export default {
           .then(() => {
             this.$modal.close(this.catId)
           })})
-      },
-      cancelMove() {
-          this.$modal.close()
       },
    },
 }
