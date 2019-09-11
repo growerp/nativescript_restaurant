3289948924d059d890724bd1f5fac88c7e6ab59d
Supatthra Nawicha <tukkata@antwebsystems.com>
2019-09-11 12:29:54 +0700

    Adjust the test script for using the emulator name as parameter.
    Change-Id: Id7a4502c15a2aac22553525f6985f2c275f11866

--- automatedTest.sh
+++ automatedTest.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
-#set -x
+
+emu=$1
+ip=$2
+
+if [ -z "$emu" ]; then
+    echo "Input emulator name (check e2e/config/appium.capabilities.json): Example $ ./automatedTest.sh Pixel_2"
+    exit
+fi
+
+if [ ! -z "$ip" ]; then
+    echo "Change test ip to $ip"
+    sed -i -e "s#10.0.2.2#$ip#g" app/services/backend-service.js
+fi
 
 echo "Remove the old packages from node_module."
 rm -rf node_modules
