837d05905016cba6e7cf49b36e2c499974ff087c
Hans Bakker <admin@antwebsystems.com>
2019-10-04 08:12:17 +0700

    style productlist with css. add search function

---
+++ app/pages/modalPages/Search.vue
@@ -0,0 +1,38 @@
+<template>
+  <page><ModalStack dismissEnabled="true" class="modal-container">
+    <StackLayout backgroundColor="white" width="80%" class="m-20 p-20">
+      <label :text="mess" class="h2 text-center" textWrap="true"/>
+      <RadDataForm :source="item" @propertyCommitted="onItemCommitted"/>
+      <GridLayout columns="*,*" rows="auto">
+        <Button class="button" :text="$t('cancel')" @tap="$modal.close(false)" col="0"/>
+        <Button class="button" :text="$t('ok')" 
+          @tap="$modal.close(editedItem)" col="1"/>
+      </GridLayout>
+    </StackLayout></ModalStack>
+  </page>
+</template>
+
+<script>
+export default {
+  name: 'Search',
+  props: {
+    message: String,
+    item: Object
+  },
+  data() {
+    return {
+      editedItem: null,
+      mess: this.message
+    }
+  },
+  methods: {
+    onItemCommitted(data) {
+      this.editedItem = JSON.parse(data.object.editedObject)
+    },
+  }
+}
+
+</script>
+
+<style>
+</style>
