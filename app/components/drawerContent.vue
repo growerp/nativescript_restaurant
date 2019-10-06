203bb55f3ef95da28624011f23810c569c51e32d
Hans Bakker <admin@antwebsystems.com>
2019-10-06 08:57:48 +0700

    Updated menu, removed 'tap' errors

--- app/components/drawerContent.vue
+++ app/components/drawerContent.vue
@@ -1,18 +1,22 @@
 <template lang="html">
   <ScrollView>
     <StackLayout width="100%">
-      <Label class="drawer-header" :text="$t('drawer')" @tap="closeDrawer()"/>
-
-      <Label
-        v-for="(page, i) in pages"
-        @tap="menuAction(page.component, page.action)"
-        class="drawer-item"
-        :text="page.title"
-        :key="i"
-        v-show="page.show==='employee' || (page.show==='admin' && $store.getters.currentEmployeeUserGroupId==='GROWERP_M_ADMIN')"
-       />
-
-      <Button class="drawer-close-button" @tap="closeDrawer()">{{ $t("closeDrawer") }}</Button>
+      <Image src='~/assets/images/growerp.png' height="70" class="m-10"
+          @tap="closeDrawer()"/>
+      <StackLayout orientation="horizontal" class="m-5"
+        v-for="(page, i) in pages" 
+        v-show="page.show==='employee' || (page.show==='admin' && 
+            $store.getters.currentEmployeeUserGroupId==='GROWERP_M_ADMIN')">
+        <Image :src="page.image" height="30"/>
+        <Label
+          @tap="menuAction(page.component, page.action)"
+          class="h3 m-5"
+          :text="page.title" textWrap="true"
+          :key="i"
+        />
+      </StackLayout>
+      <Button class="drawer-close-button" 
+        @tap="closeDrawer()">{{ $t("closeDrawer") }}</Button>
     </StackLayout>
   </ScrollView>
 </template>
@@ -28,21 +32,32 @@ export default {
     return {
       // define our pages, making sure the component matches that defined in /app/router/index.js
       pages: [
-        { title: this.$t("operationalMenuTitle"), show: 'admin'},
-        { name: 'Home', title: this.$t('dashBoard'), component: this.$routes.Home, action: '', show: 'employee'},
-        { name: 'Orders', title: this.$t('orders'), component: this.$routes.Orders, action: '', show: 'employee'},
-        { name: 'Prepare', title: this.$t('preparation'), component: this.$routes.Prepare, action: '', show: 'employee'},
-        { name: 'Reports', title: this.$t('reports'), component: this.$routes.Reports, action: '', show: 'employee'},
-        { name: 'Help', title: this.$t('help'), component: this.$routes.Help, action: '', show: 'employee'},
-        { name: 'Tasks', title:this.$t('tasks'), component: this.$routes.Tasks, action: '', show: 'employee'},
-        { name: 'MyInfo', title: this.$t('myInfo'),  component: this.$routes.MyInfo, action: '', show: 'employee'},
-        { name: 'Logout', title: this.$t('logout'),  component: this.$routes.Login, action: 'Logout', show: 'employee'},
-        { title: this.$t("setUpMenuTitle"), show: 'admin' },
-        { name: 'Organization', title: this.$t('companyEmplCust'), component: this.$routes.Organization, action: '', show: 'admin'},
-        { name: 'Locations', title: this.$t('locations'), component: this.$routes.Locations, action: '', show: 'admin'},
-        { name: 'Products', title: this.$t('products'), component: this.$routes.Products, action: '', show: 'admin'},
-        { name: 'Categories', title: this.$t('categories'), component: this.$routes.Categories, action: '', show: 'admin'},
-        { name: 'About', title: this.$t('about'), component: this.$routes.About, action: '', show: 'admin'},
+        { image: '~/assets/images/home.png', title: this.$t('dashBoard'),
+            component: this.$routes.Home, action: '', show: 'employee'},
+        { image: '~/assets/images/waiter.png', title: this.$t('orders'),
+            component: this.$routes.Orders, action: '', show: 'employee'},
+        { image: '~/assets/images/prep.png', title: this.$t('preparation'), 
+            component: this.$routes.Prepare, action: '', show: 'employee'},
+        { image: '~/assets/images/report.png', title: this.$t('reports'), 
+            component: this.$routes.Reports, action: '', show: 'employee'},
+        { image: '~/assets/images/help.png', title: this.$t('help'),
+            component: this.$routes.Help, action: '', show: 'employee'},
+        { image: '~/assets/images/task.png', title:this.$t('tasks'),
+            component: this.$routes.Tasks, action: '', show: 'employee'},
+        { image: '~/assets/images/myInfo.png', title: this.$t('myInfo'), 
+            component: this.$routes.MyInfo, action: '', show: 'employee'},
+        { image: '~/assets/images/logout.png', title: this.$t('logout'),
+            component: this.$routes.Login, action: 'Logout', show: 'employee'},
+        { image: '~/assets/images/company.png', title: this.$t('companyEmplCust'),
+            component: this.$routes.Organization, action: '', show: 'admin'},
+        { image: '~/assets/images/locations.png', title: this.$t('locations'),
+            component: this.$routes.Locations, action: '', show: 'admin'},
+        { image: '~/assets/images/product.png', title: this.$t('products'),
+            component: this.$routes.Products, action: '', show: 'admin'},
+        { image: '~/assets/images/category.png', title: this.$t('categories'),
+            component: this.$routes.Categories, action: '', show: 'admin'},
+        { image: '~/assets/images/about.png', title: this.$t('about'), 
+            component: this.$routes.About, action: '', show: 'admin'},
       ]
     }
   },
