f3b0a567d9614d6617378937e6251064995cff15
Hans Bakker <admin@antwebsystems.com>
2019-10-15 11:29:13 +0700

    action bar giving warning messages

--- app/components/actionBar.vue
+++ app/components/actionBar.vue
@@ -29,9 +29,9 @@
     name: 'myActionBar',
     props: {
       back: {type: Boolean, default: false},
-      openDrawer: {type: Function, default: null},
-      onHeaderTap: {type: Function, default: null},
-      onActionTap: {type: Function, default: null},
+      openDrawer: {type: Function, default: function(){}},
+      onHeaderTap: {type: Function, default: function(){}},
+      onActionTap: {type: Function, default: function(){}},
       save: {type: Boolean, default: false},
       plus: {type: Boolean, default: false},
       reload: {type: Boolean, default: false},
